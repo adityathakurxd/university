96732e8addd5aa69707902a523fe683112a23c8a
hrigved-dev <hrigved.khatavkar@gmail.com>
2021-10-01 21:41:58 +0530

    Sign In Screen Changes

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MentorWidget(),
+      home: SignInScreen(),
     );
     // return FutureBuilder(
     //   future: Firebase.initializeApp(),
